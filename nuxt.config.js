f58bf310c4507326c28fb3761cf9a18da0277756
yama-choki <lovepingpong0@gmail.com>
2021-12-19 22:24:08 +0900

    ヘッダーの作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,11 +32,11 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-     // Simple usage
-     '@nuxtjs/vuetify',
+    // Simple usage
+    '@nuxtjs/vuetify',
 
-     // With options
-     ['@nuxtjs/vuetify', { /* module options */ }]
+    // With options
+    ['@nuxtjs/vuetify', { /* module options */ }]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,45 @@
 <template>
-  <Tutorial/>
+  <v-app>
+    <body>
+      <header>
+        <v-app-bar
+          color="#00CCCC"
+          dark
+          max-width="1263px"
+          app
+        >
+          <v-toolbar-title>
+            Port
+          </v-toolbar-title>
+
+          <v-spacer />
+        </v-app-bar>
+      </header>
+      <section id="container">
+        <main></main>
+      </section>      
+    </body>
+  </v-app>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+body{
+  max-width: 1263px;
+  margin: 0 auto;
+}
+main{
+  margin: 0 auto;
+}
+header{
+  margin: 0 auto;
+}
+
+#container{
+  margin-top: 64px;
+  display:flex
+}
+</style>
