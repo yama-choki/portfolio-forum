1fd19fd9f8b0b50d5915a1682756d24780c16596
yama-choki <lovepingpong0@gmail.com>
2021-12-19 22:38:23 +0900

    投稿機能の見た目をコンポーネント化

--- components/Header.vue
+++ components/Header.vue
@@ -11,105 +11,15 @@
       </v-toolbar-title>
 
       <v-spacer />
-      <v-dialog
-      v-model="postDialog"
-      persistent
-      max-width="600px"
-    >
-      <template #activator="{ on, attrs }">
-        <v-btn
-          icon
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          <v-icon>mdi-pencil-plus</v-icon>
-        </v-btn>
-      </template>
-      <v-card>
-        <form>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              color="blue darken-1"
-              text
-              @click="postDialog = false"
-            >
-              キャンセル
-            </v-btn>
-            <button>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="postDialog = false, add()"
-              >
-                投稿する
-              </v-btn>
-            </button>
-          </v-card-actions>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col
-                  cols="12"
-                  sm="12"
-                  md="12"
-                >
-                  <v-textarea
-                    v-model="post.text"
-                    label="ポートフォリオの説明"
-                    required
-                    counter="200"
-                  />
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="12"
-                  md="12"
-                >
-                  <v-text-field
-                    v-model="post.portfolioUrl"
-                    label="ポートフォリオ"
-                    hint="URLを添付してください"
-                    persistent-hint
-                  />
-                </v-col>
-                <v-col
-                  cols="12"
-                  sm="12"
-                >
-                  <v-select
-                    v-model="post.category"
-                    :items="['Webアプリ', 'Webデザイン','動画編集', 'イラスト', 'その他']"
-                    label="ポートフォリオの種類"
-                    required
-                  />
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-        </form>
-      </v-card>
-    </v-dialog>
+      <div>
+        <AddPost />
+      </div>
     </v-app-bar>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      postDialog: false,
-      post: {
-        text: '',
-        portfolioUrl: '',
-        snsAccount: '',
-        category: '',
-        created: '',
-        user: ''
-      }
-    }
-  }
 }
 </script>
 
