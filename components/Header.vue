2fee5d073af5f7efc52bea7dde911110f6d33296
yama-choki <lovepingpong0@gmail.com>
2021-12-20 23:59:50 +0900

    ログイン機能の見た目をコンポーネント化

--- components/Header.vue
+++ components/Header.vue
@@ -11,69 +11,15 @@
       </v-toolbar-title>
 
       <v-spacer />
+        <Login />
         <AddPost />
-        <div id="login">
-          <div class="text-center">
-            <v-dialog
-              v-model="loginDialog"
-              width="500"
-            >
-              <template #activator="{ on, attrs }">
-                <v-btn
-                  dark
-                  v-bind="attrs"
-                  icon
-                  v-on="on"
-                >
-                  <v-icon>mdi-login</v-icon>
-                </v-btn>
-              </template>
-
-              <v-card>
-                <v-card-title class="text-h5 grey lighten-2">
-                  ログイン
-                </v-card-title>
-
-                <div class="container">
-                  <!-- <p class="title is-1 is-spaced">user:{{ $store.getters['login/getLoginUser'] }}</p> -->
-                  <!-- <button @click="loginGoogle()">
-                    Google ログイン
-                  </button> -->
-                  <button @click="loginTwitter()">
-                    Twitter ログイン
-                  </button>
-                  <!-- <button @click="loginFacebook()">
-                    Facebook ログイン
-                  </button> -->
-                </div>
-
-                <v-divider />
-
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn
-                    color="primary"
-                    text
-                    @click="loginDialog = false"
-                  >
-                    I accept
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </div>
-        </div>
     </v-app-bar>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      loginDialog: false
-    }
-  },
+  
 }
 </script>
 
---
+++ components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="login">
+    <div class="text-center">
+      <v-dialog v-model="loginDialog" width="500">
+        <template #activator="{ on, attrs }">
+          <v-btn dark v-bind="attrs" icon v-on="on">
+            <v-icon>mdi-login</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2"> ログイン </v-card-title>
+
+          <div class="container">
+            <!-- <p class="title is-1 is-spaced">user:{{ $store.getters['login/getLoginUser'] }}</p> -->
+            <!-- <button @click="loginGoogle()">
+            Google ログイン
+          </button> -->
+            <button @click="loginTwitter()">Twitter ログイン</button>
+            <!-- <button @click="loginFacebook()">
+            Facebook ログイン
+          </button> -->
+          </div>
+
+          <v-divider />
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="primary" text @click="loginDialog = false">
+              I accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginDialog: false
+    }
+  },
+};
+</script>
+
+<style>
+.container{
+  text-align: center;
+}
+</style>
