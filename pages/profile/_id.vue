961446f6d69b48c1ecb9139b5b613db8adf07201
yama-choki <lovepingpong0@gmail.com>
2022-02-11 23:08:09 +0900

    プロフィールページのファイル構造を変更

---
+++ pages/profile/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <section>
+      <h1>{{ message }}</h1>
+      <p></p>
+    </section>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: '_profile.vueを表示'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
