2296111a2c311608079935e81ffe3de1864c10cd
yama-choki <lovepingpong0@gmail.com>
2022-04-06 18:58:26 +0900

    npm buildを実行

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // Simple usage
-    '@nuxtjs/vuetify',
+    // '@nuxtjs/vuetify',
 
     // With options
     ['@nuxtjs/vuetify', { /* module options */ }]
